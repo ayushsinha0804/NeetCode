9e9e858c0744409a3103fa73f83d35baf78c5827
Ayush Sinha <73655379+ayushsinha0804@users.noreply.github.com>
2024-07-08 22:16:40 +0530

    .

---
+++ groupAnagrams.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class groupAnagrams {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of strings: ");
+        int len=sc.nextInt();
+        String arr[]=new String[len];
+        for(int i=0;i<len;i++){
+            arr[i]=sc.next();
+        }
+        List<List<String>> res = new ArrayList<>();
+        res=groupAnagram(arr);
+        System.out.println(res);
+        sc.close();
+    }
+    public static List<List<String>> groupAnagram(String arr[]){
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : arr) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
