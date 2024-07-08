81c517183b1fda7605b444a7894f4aa8c7e1d6cb
Ayush Sinha <73655379+ayushsinha0804@users.noreply.github.com>
2024-07-08 21:39:41 +0530

    2nd code

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -5,6 +5,7 @@ public class containsDuplicate{
         System.out.println("Enter the length of array: ");
         int len=sc.nextInt();
         int arr[]=new int[len];
+        System.out.println("Enter the array: ");
         for(int i=0;i<len;i++){
             arr[i]=sc.nextInt();
         }
---
+++ validAanagrams.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class validAanagrams {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first string: ");
+        String s1=sc.nextLine();
+        System.out.println("Enter the second string: ");
+        String s2=sc.nextLine();
+        if(isValid(s1,s2)){
+            System.out.println("The given two strings are anagrams.");
+        }
+        else{
+            System.out.println("The given two strings are not anagrams.");
+        }
+    }
+    public static boolean isValid(String s1, String s2){
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        int arr[]=new int[26];
+        for(int i=0;i<s1.length();i++){
+            arr[s1.charAt(i) - 'a']++;
+            arr[s2.charAt(i) - 'a']--;
+        }
+        for(int n:arr){
+            if(n!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
