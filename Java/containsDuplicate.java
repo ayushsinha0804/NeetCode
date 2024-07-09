c19a987dc7fb0b27fa56878af9c0237fabafcd3b
Ayush Sinha <73655379+ayushsinha0804@users.noreply.github.com>
2024-07-09 23:08:46 +0530

    change directory

---
+++ Java/containsDuplicate.java
@@ -0,0 +1,31 @@
+package Java;
+import java.util.*;
+public class containsDuplicate{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the length of array: ");
+        int len=sc.nextInt();
+        int arr[]=new int[len];
+        System.out.println("Enter the array: ");
+        for(int i=0;i<len;i++){
+            arr[i]=sc.nextInt();
+        }
+        if(check(arr)){
+            System.out.println("The array contains duplicate elements.");
+        }
+        else{
+            System.out.println("The array doesn't containe duplicate elements.");
+        }
+        sc.close();
+    }
+    public static boolean check(int arr[]){
+        Set <Integer> se=new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            if(se.contains(arr[i])){
+                return true;
+            }
+            se.add(arr[i]);
+        }
+        return false;
+    }
+}
